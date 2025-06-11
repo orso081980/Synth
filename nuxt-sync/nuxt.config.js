bd67b7d4f79247138c49006a698d00eaed1a309d
Marcoatisabel <orsacchiotto80@hotmail.com>
2025-06-11 18:26:01 +0200

    test

--- nuxt-sync/nuxt.config.js
+++ nuxt-sync/nuxt.config.js
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  nitro: {
+    preset: 'static', // 💡 this is key!
+  },
+  app: {
+    baseURL: '/Synth/', // 💡 repo name here!
+  }
 })
